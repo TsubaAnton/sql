e7ea93a91dc2d87700077d15fb6ae2ffaeb8ec8a
TsubaAnton <antontsuba2003@gmail.com>
2024-02-09 03:56:09 +0200

    Homework3

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,16 +1,31 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-
+SELECT c.company_name, first_name, last_name
+FROM orders as o
+JOIN customers as c USING(customer_id)
+JOIN employees as e USING(employee_id)
+JOIN shippers as s ON o.ship_via  = s.shipper_id
+WHERE c.city = 'London' AND e.city = 'London' AND s.company_name = 'United Package'
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
-
+SELECT p.product_name, p.units_in_stock, s.contact_name, s.phone
+FROM products as p
+JOIN suppliers as s ON p.supplier_id  = s.supplier_id
+JOIN categories as c ON p.category_id = c.category_id
+WHERE p.discontinued = 0 AND p.units_in_stock < 25
+AND c.category_name IN ('Dairy Products', 'Condiments')
+ORDER BY units_in_stock
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
-
+SELECT company_name FROM customers
+WHERE customer_id NOT IN (SELECT DISTINCT customer_id FROM orders)
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+SELECT DISTINCT product_name FROM products
+WHERE product_id IN (SELECT product_id FROM order_details WHERE quantity = 10)
+
